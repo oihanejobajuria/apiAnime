0a07db611d08fba898a9ea7244e00e51986656b1
iam23921514 <oihanejobajuria@gmail.com>
2021-11-24 18:05:48 +0100

    24nov clase

---
+++ src/main/java/com/example/anime/domain/model/Users.java
@@ -0,0 +1,15 @@
+package com.example.anime.domain.model;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public UUID animeid;
+
+    public String username;
+    public String password;
+}
---
+++ src/main/java/com/example/anime/repository/UsersRespository.java
@@ -0,0 +1,13 @@
+package com.example.anime.repository;
+
+import com.example.anime.domain.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+//public class UsersRespository extends JpaRepository<Users, UUID> {
+//    Users findByUserName(String username);
+//}
+
+public class UsersRespository {
+}
