d929699f7fd4a812d838c0e09deb93669430c712
oihanejobajuria <oihanejobajuria>
2022-01-19 18:38:18 +0100

    19ene clase

---
+++ build/resources/main/db/migration/V1__createTables.sql
@@ -0,0 +1,57 @@
+--  V1 -------------------------------------------------------------------------------------
+CREATE TABLE anime(
+    animeid UUID not null default gen_random_uuid() primary key,
+    name text,
+    description text,
+    type text,
+    year int,
+    imageurl text
+);
+
+CREATE TABLE users(
+    usersid UUID not null default gen_random_uuid() primary key,
+    username varchar(24) NOT NULL UNIQUE,
+    password varchar(255) NOT NULL,
+    role varchar(10),
+    enabled boolean DEFAULT true
+);
+
+CREATE TABLE file (
+    fileid UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    contenttype text,
+    data bytea
+);
+
+--  V2 -------------------------------------------------------------------------------------
+
+CREATE TABLE authors(
+    authorid UUID not null default gen_random_uuid() primary key,
+    name text,
+    imageurl text
+);
+
+CREATE TABLE anime_author(
+    animeid UUID REFERENCES anime(animeid) ON DELETE CASCADE,
+    authorid UUID REFERENCES authors(authorid) ON DELETE CASCADE,
+    PRIMARY KEY (animeid, authorid)
+);
+
+CREATE TABLE genres(
+    genreid UUID not null default gen_random_uuid() primary key,
+    label text,
+    imageurl text
+);
+
+CREATE TABLE anime_genre(
+    animeid UUID REFERENCES anime(animeid) ON DELETE CASCADE,
+    genreid UUID REFERENCES genres(genreid) ON DELETE CASCADE,
+    PRIMARY KEY (animeid, genreid)
+);
+
+--  V3 -------------------------------------------------------------------------------------
+
+CREATE TABLE favorites(
+  animeid UUID REFERENCES anime(animeid) ON DELETE CASCADE,
+  usersid UUID REFERENCES users(usersid) ON DELETE CASCADE,
+  PRIMARY KEY (animeid, usersid)
+);
---
+++ build/resources/main/db/migration/V2__insertarDatos.sql
@@ -0,0 +1,62 @@
+--  V1 -------------------------------------------------------------------------------------
+INSERT INTO anime(name, description, type, year, imageurl) values
+    ('Fullmetal Alchemist', 'FMA', 'action', 2003, '/images/123'),
+    ('Fullmetal Alchemist Brotherhood', 'FMAB', 'action', 2009, '/images/124 '),
+    ('Haikyuu!', 'Voley', 'deportes', 2014, '/images/124 '),
+    ('Hunter X Hunter', 'HXH', 'action', 2011, '/images/124 '),
+    ('Black Clover', 'BC', 'action', 2017, '/images/124 '),
+    ('Jibaku Shonen Hanako-kun', 'Hanako', 'sobrenatural', 2020, '/images/124 '),
+    ('Cardcaptor Sakura', 'Sakura', 'magia', 2000, '/images/124 ');
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+INSERT INTO users (username, password) VALUES
+    ('user', crypt('pass', gen_salt('bf'))),
+    ('osj', crypt('pass', gen_salt('bf')));
+
+-- file
+
+--  V2 -------------------------------------------------------------------------------------
+
+INSERT INTO authors(name, imageurl) values
+    ('Hiromu Arakawa', '/images/123'),
+    -- ('Hiromu Arakawa', '/images/123'), -- fmab = fma
+    ('Haruichi Furudate', '/images/123'),
+    ('Yoshihiro Togashi', '/images/123'),
+    ('Yuki Tabata', '/images/123'),
+    ('Iro Aida', '/images/123'),
+    ('Clamp', '/images/123');
+
+INSERT INTO anime_author VALUES
+    ((SELECT animeid FROM anime WHERE name='Fullmetal Alchemist'),(SELECT authorid FROM authors WHERE name='Hiromu Arakawa')),
+    ((SELECT animeid FROM anime WHERE name='Fullmetal Alchemist Brotherhood'),(SELECT authorid FROM authors WHERE name='Hiromu Arakawa')),
+    ((SELECT animeid FROM anime WHERE name='Haikyuu!'),(SELECT authorid FROM authors WHERE name='Haruichi Furudate')),
+    ((SELECT animeid FROM anime WHERE name='Hunter X Hunter'),(SELECT authorid FROM authors WHERE name='Yoshihiro Togashi')),
+    ((SELECT animeid FROM anime WHERE name='Black Clover'),(SELECT authorid FROM authors WHERE name='Yuki Tabata')),
+    ((SELECT animeid FROM anime WHERE name='Jibaku Shonen Hanako-kun'),(SELECT authorid FROM authors WHERE name='Iro Aida')),
+    ((SELECT animeid FROM anime WHERE name='Cardcaptor Sakura'),(SELECT authorid FROM authors WHERE name='Clamp'));
+
+INSERT INTO genres(label, imageurl) values
+    ('action',  'img/1'),
+    ('deportes',  'img/1'),
+    ('sobrenatural',  'img/1'),
+    ('magia',  'img/1');
+
+INSERT INTO anime_genre VALUES
+    ((SELECT animeid FROM anime WHERE name='Fullmetal Alchemist'),(SELECT genreid FROM genres WHERE label='action')),
+    ((SELECT animeid FROM anime WHERE name='Fullmetal Alchemist Brotherhood'),(SELECT genreid FROM genres WHERE label='action')),
+    ((SELECT animeid FROM anime WHERE name='Haikyuu!'),(SELECT genreid FROM genres WHERE label='deportes')),
+    ((SELECT animeid FROM anime WHERE name='Hunter X Hunter'),(SELECT genreid FROM genres WHERE label='action')),
+    ((SELECT animeid FROM anime WHERE name='Black Clover'),(SELECT genreid FROM genres WHERE label='action')),
+    ((SELECT animeid FROM anime WHERE name='Jibaku Shonen Hanako-kun'),(SELECT genreid FROM genres WHERE label='sobrenatural')),
+    ((SELECT animeid FROM anime WHERE name='Cardcaptor Sakura'),(SELECT genreid FROM genres WHERE label='magia'));
+
+--  V3 -------------------------------------------------------------------------------------
+
+INSERT INTO favorites VALUES
+    ((SELECT animeid FROM anime WHERE name='Fullmetal Alchemist'),(SELECT usersid FROM users WHERE username='user')),
+    ((SELECT animeid FROM anime WHERE name='Fullmetal Alchemist Brotherhood'),(SELECT usersid FROM users WHERE username='user')),
+    ((SELECT animeid FROM anime WHERE name='Haikyuu!'),(SELECT usersid FROM users WHERE username='user')),
+    ((SELECT animeid FROM anime WHERE name='Hunter X Hunter'),(SELECT usersid FROM users WHERE username='user')),
+    ((SELECT animeid FROM anime WHERE name='Black Clover'),(SELECT usersid FROM users WHERE username='user')),
+    ((SELECT animeid FROM anime WHERE name='Jibaku Shonen Hanako-kun'),(SELECT usersid FROM users WHERE username='user')),
+    ((SELECT animeid FROM anime WHERE name='Cardcaptor Sakura'),(SELECT usersid FROM users WHERE username='user'));
