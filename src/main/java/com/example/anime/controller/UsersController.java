6a784fdc58077fe015eced3f27f146048a26f59c
oihanejobajuria <oihanejobajuria>
2022-01-21 20:36:57 +0100

    v3

--- src/main/java/com/example/anime/controller/UsersController.java
+++ src/main/java/com/example/anime/controller/UsersController.java
@@ -6,13 +6,12 @@ import com.example.anime.domain.dto.Error;
 import com.example.anime.domain.model.Anime;
 import com.example.anime.domain.model.Favorite;
 import com.example.anime.domain.model.Users;
-import com.example.anime.domain.model.projection.ProjectionAnime_idNameImg;
+import com.example.anime.domain.model.projection.ProjectionFav_animeidUserid;
 import com.example.anime.domain.model.projection.ProjectionFav_setAnime;
 import com.example.anime.domain.model.projection.ProjectionUsers_idUsername;
 import com.example.anime.repository.AnimeRepository;
 import com.example.anime.repository.FavoriteRepository;
 import com.example.anime.repository.UsersRespository;
-import org.apache.catalina.User;
 import org.springframework.security.core.Authentication;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +19,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
@@ -99,29 +97,76 @@ public class UsersController {
         if (authentication.getName() != null) {
             Users autorizado = usersRepository.findByUsername(authentication.getName());
 
-            Favorite f = new Favorite();
-            f.animeid = requestFavorite.animeid;
-            f.usersid = autorizado.usersid;
-            favoriteRepository.save(f);
-            return ResponseEntity.ok()
-                    .body( usersRepository.findByUsername(authentication.getName(), ProjectionFav_setAnime.class) );
+            boolean estaAnime=false, estaFavs=false;
+            for (Anime a : animeRepository.findAll()){
+                if(a.animeid.equals(requestFavorite.animeid)) {
+                    estaAnime = true;
+                }
+            }
+            for (Favorite f : favoriteRepository.findAll()) {
+                if (f.animeid.equals(requestFavorite.animeid)){
+                    estaFavs = true;
+                }
+            }
+
+            if(!estaAnime){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body( Error.message("Aquesta id no perteneix a cap anime existent") );
+            } else {
+                if (estaFavs) {
+                    return ResponseEntity.status(HttpStatus.CONFLICT).body( Error.message("Aquest anime ja esta en favorits") );
+                } else {
+                    Favorite f = new Favorite();
+                    f.animeid = requestFavorite.animeid;
+                    f.usersid = autorizado.usersid;
+                    favoriteRepository.save(f);
+
+                    return ResponseEntity.ok()
+                            .body( f );
+                }
+            }
+
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body( Error.message("No estas autoritzat") );
 
     }
 
-//    @DeleteMapping("/favorites/{id}")
-//    public ResponseEntity<?> deleteUsers(@PathVariable UUID id){
-//        Users u = usersRepository.findById(id).orElse(null);
-//
-//        if (u == null) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-//                    .body(Error.message("No s'ha trobat el user amd id '" + id  + "'"));
-//        }
-//        usersRepository.delete(u);
-//        return ResponseEntity.ok()
-//                .body( Error.message( "S'ha eliminat l'usuari amd id '" + id + "'" ));
-//    }
+    @DeleteMapping("/favorites/{id}")
+    public ResponseEntity<?> deleteFav(@PathVariable UUID id, Authentication authentication){
+
+        if (authentication.getName() != null) {
+            Users autorizado = usersRepository.findByUsername(authentication.getName());
+
+            boolean estaAnime=false, estaFavs=false;
+            for (Anime a : animeRepository.findAll()){
+                if(a.animeid.equals(id)) {
+                    estaAnime = true;
+                }
+            }
+            for (Favorite f : favoriteRepository.findAll()) {
+                if (f.animeid.equals(id)){
+                    estaFavs = true;
+                }
+            }
+
+            if(!estaAnime){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body( Error.message("Aquesta id no perteneix a cap anime existent") );
+            } else {
+                if (!estaFavs) {
+                    return ResponseEntity.status(HttpStatus.CONFLICT).body( Error.message("Aquest anime no esta en favorits") );
+                } else {
+                    Favorite f = new Favorite();
+                    f.animeid = id;
+                    f.usersid = autorizado.usersid;
+                    favoriteRepository.delete(f);
+
+                    return ResponseEntity.ok()
+                            .body( Error.message( "S'ha eliminat del favorits l'anime amb id '" + id + "'" ) );
+                }
+            }
+
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body( Error.message("No estas autoritzat") );
+    }
 
 
 
