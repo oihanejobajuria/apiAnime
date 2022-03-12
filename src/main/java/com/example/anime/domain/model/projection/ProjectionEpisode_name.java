cc9cc004b931b967578adf1c2ef533a67eab86ae
Oihane <oihanejobajuria@gmail.com>
2022-03-12 18:31:54 +0100

    changes projections 3

--- src/main/java/com/example/anime/domain/model/projection/ProjectionEpisode_name.java
+++ src/main/java/com/example/anime/domain/model/projection/ProjectionEpisode_name.java
@@ -1,6 +1,8 @@
 package com.example.anime.domain.model.projection;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonPropertyOrder({"name", "num"})
 public interface ProjectionEpisode_name {
     String getName();
     int getNum();
